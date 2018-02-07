557c17464cab6ef819679aa8d5005b589eb59d9a
Mike Smyk <mike.smyk@advisori.de>
2018-02-07 14:20:18 +0100

    Fix script

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,3 @@
-#  You will be required to submit: 
-#    1) a tidy data set as described below, 
-#    2) a link to a Github repository with your script for performing the analysis, and 
-#    3) a code book that describes the variables, the data, and any transformations or work 
-#       that you performed to clean up the data called CodeBook.md. 
-#       You should also include a README.md in the repo with your scripts. 
-#       This repo explains how all of the scripts work and how they are connected.
-
-
-## 
-# You should create one R script called run_analysis.R that does the following.
-#
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names.
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of 
-#    each variable for each activity and each subject.
-
 rm(list=ls())
 
 library(dplyr)
@@ -59,4 +40,4 @@ tidy_data <- data %>%
   group_by(activity, subject) %>%
   summarize_all(funs(mean(., na.rm=TRUE)))
              
-write.csv(tidy_data, 'tidy_data.csv', row.names=FALSE)
+write.table(tidy_data, 'tidy_data.txt', row.names=FALSE)
